06496578497f7ed8d6665061587a77cf290b418c
enzo <enzomarin12@gmail.com>
2022-09-01 13:22:46 -0400

    Conexion a MDB, HTML, CSS, jS

--- app.js
+++ app.js
@@ -2,24 +2,49 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
 const path = require('path')
 
+const app = express()
+
 mongoose.connect( // .connect retorna una promesa
-    `mongodb+srv://enzom:${process.env.DB_MONGO_PASS}@develpment.qecsrsv.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://enzom:${process.env.DB_MONGO_PASS}@develpment.qecsrsv.mongodb.net/stock-app?retryWrites=true&w=majority`
     ).then(result => console.log("Conexion a BD exitosa")) 
     .catch(err => console.log(err))
 
 // midleware 
 app.use(express.static(path.join(__dirname,'public')))
 
+// Definimos el esquema de la base de datos
+const productSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    price: Number
+},
+{timestamps : true}// automaticamente se crean dos campos createdAt updatedAt
+)
 
-/*
-app.get('/', (req,res) =>{
-    console.log('Peticion recibida!');
-    res.status(200).send('<h1>Hola mundo</>')
+// modelo
+const Product =mongoose.model('Product', productSchema)
+
+
+
+app.use(express.json())
+
+app.post('/api/v1/products', (req,res) =>{
+    const newProduct = new Product({
+        name: req.body.name,
+        price: req.body.price
+    })
+
+    newProduct
+        .save()
+        .then(result =>{
+            res.status(201).json({ok:true})
+        })
+        .catch(err => console.log(err))
+    //console.log({body: req.body})
+   
 })
-*/
+
 
 
 const PORT = process.env.PORT || 4000
--- public/index.js
+++ public/index.js
@@ -1,5 +1,25 @@
+
 console.log('console log desde el navegador')
 
 const productName = document.querySelector('#productName')
 const productPrice = document.querySelector('#productPrice')
-console.log({productName,productPrice})
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', (e) =>{
+    //console.log({name: productName.value, price: productPrice.value})
+    const name = productName.value
+    const price = productPrice.value
+
+    //realizamos una peticion al servidor con fetch
+    fetch('/api/v1/products',{
+        method: "POST", 
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            price
+        }),
+    })
+})
