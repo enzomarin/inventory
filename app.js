a097d41df81073d9d2cd5676586c0bb01550ef4b
enzo <enzomarin12@gmail.com>
2022-09-01 00:28:05 -0400

    base de datos

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 
 require('dotenv').config()
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
+const path = require('path')
 
+mongoose.connect( // .connect retorna una promesa
+    `mongodb+srv://enzom:${process.env.DB_MONGO_PASS}@develpment.qecsrsv.mongodb.net/?retryWrites=true&w=majority`
+    ).then(result => console.log("Conexion a BD exitosa")) 
+    .catch(err => console.log(err))
+
+// midleware 
+app.use(express.static(path.join(__dirname,'public')))
+
+
+/*
 app.get('/', (req,res) =>{
     console.log('Peticion recibida!');
     res.status(200).send('<h1>Hola mundo</>')
 })
+*/
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
---
+++ public/index.js
@@ -0,0 +1,5 @@
+console.log('console log desde el navegador')
+
+const productName = document.querySelector('#productName')
+const productPrice = document.querySelector('#productPrice')
+console.log({productName,productPrice})
