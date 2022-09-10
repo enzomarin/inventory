c9095115cdb5576c4059e1d55424702c6b31b97f
enzo <enzomarin12@gmail.com>
2022-09-10 18:38:16 -0300

    post

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 
 require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
+//const axios = require('axios')
+
+app.use(cors())
 
 mongoose.connect( // .connect retorna una promesa
     `mongodb+srv://enzom:${process.env.DB_MONGO_PASS}@develpment.qecsrsv.mongodb.net/stock-app?retryWrites=true&w=majority`
     ).then(result => console.log("Conexion a BD exitosa")) 
     .catch(err => console.log(err))
 
-// midleware 
-app.use(express.static(path.join(__dirname,'public')))
 
 // Definimos el esquema de la base de datos
 const productSchema = mongoose.Schema({
@@ -30,6 +32,7 @@ const Product =mongoose.model('Product', productSchema)
 app.use(express.json())
 
 app.post('/api/v1/products', (req,res) =>{
+
     const newProduct = new Product({
         name: req.body.name,
         price: req.body.price
@@ -41,10 +44,25 @@ app.post('/api/v1/products', (req,res) =>{
             res.status(201).json({ok:true})
         })
         .catch(err => console.log(err))
+    
     //console.log({body: req.body})
    
 })
+/*
+app.get('/', (req, res, next) =>{
+    const pokeApiBaseUrl = 'https://pokeapi.co/api/v2/pokemon'
 
+    axios(`${pokeApiBaseUrl}/charmander`).then( (res) =>{
+        const pokemon = res.data
+        console.log({pokemon})
+    })
+
+    next()
+})
+*/
+
+// midleware 
+//app.use(express.static(path.join(__dirname,'public')))
 
 
 const PORT = process.env.PORT || 4000
--- public/index.js
+++ public/index.js
@@ -5,6 +5,27 @@ const productName = document.querySelector('#productName')
 const productPrice = document.querySelector('#productPrice')
 
 const button = document.querySelector('button')
+const pokeApiBaseUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+fetch(`${pokeApiBaseUrl}/charmander`, {
+    method: "GET",
+
+}).then((res)=>{
+    return res.json()
+}).then((pokemon) =>{
+    console.log({pokemon})
+
+    const html = `
+        <h3>${pokemon.name}</h3>
+        <img src=${pokemon.sprites.front_default} alt="Imagen de: ${pokemon.name}">
+        <span>id: #${pokemon.id}</span>
+    `
+    const div = document.createElement('div')
+    div.classList.add('poke-card')
+    div.innerHTML = html
+    document.querySelector('body').appendChild(div)
+}).catch()
+
 
 button.addEventListener('click', (e) =>{
     //console.log({name: productName.value, price: productPrice.value})
